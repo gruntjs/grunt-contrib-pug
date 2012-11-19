cb1915314ae96c5ed8aadbc76e616ad28c10c4c0
Tyler Kellen <tyler@sleekcode.net>
2012-11-19 16:19:16 -0500

    fix failing tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -56,15 +56,16 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
   grunt.loadNpmTasks('grunt-contrib-internal');
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['nodeunit']);
+  grunt.registerTask('test', ['jade', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jade', 'test', 'build-contrib']);
+  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
 
 };
