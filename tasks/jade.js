ef4312060950df4e40a9b05b94f25c72f22710ff
Joe Longstreet <joelongstreet@gmail.com>
2013-01-08 16:50:53 -0600

    pasting in a hefty portion of Troy Goode\'s connect-assets-jade module

--- tasks/jade.js
+++ tasks/jade.js
@@ -69,5 +69,32 @@ module.exports = function(grunt) {
 
   var compileJadeTemplate = function(srcCode, srcFile, basePath, options){
     var templateName = options.templatePath + srcFile.replace(basePath, '').replace('.jade', '');
+
+    // get template function
+    var js = jade.compile(srcCode, options).toString();
+
+    // needed for jade runtime
+    var attrs = jade.runtime.attrs.toString();
+    var escape = jade.runtime.escape.toString();
+    var rethrow = jade.runtime.rethrow.toString();
+    var merge = jade.runtime.merge.toString();
+    var runtime = (escape + attrs + rethrow + merge).replace(/exports\./g, '');
+
+    // needed to make things work...
+    js = 'if(window.' + options.templatesArray + ' === undefined){window.' + options.templatesArray + '={};}\n' + js;
+    js = js.replace('function anonymous', options.templatesArray + '[\'' + templateName + '\'] = function');
+    js = js.replace(/buf = \[\];/, 'buf = [];' + runtime + '\n'); // make the runtime functions available
+    js = js.replace(/^attrs.*$/gm, ''); // attrs line references "jade" object that doesn't exist
+
+    // remove debug statements and format code nicely
+    if(!options.debug){
+      js = js.replace(/^var __jade.*$/gm, ''); // get rid of __jade debug object
+      js = js.replace(/^try.*$/gm, ''); // get rid of outer try block
+      js = js.replace(/^__jade\..*$\n/gm, ''); // git rid of debug statements
+      js = js.replace(/\n{2,}/g, ''); // get rid of blank lines
+      js = js.replace('{var buf', '{\nvar buf'); // the above line is slight greedier than I want it
+      js = js.substring(0, js.indexOf('} catch')) + '};'; // get rid of catch block, since there is no try
+    }
+    return js;
   };
 };
