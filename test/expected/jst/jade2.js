ee1e0d19628c91b385910afb08ec8f17dc9f0662
Jaime Pillora <jpillora@gmail.com>
2013-05-08 11:35:32 +1000

    updated expected test output

--- test/expected/jst/jade2.js
+++ test/expected/jst/jade2.js
@@ -1,16 +1,14 @@
 this["JST"] = this["JST"] || {};
 
-this["JST"]["jade2"] = function anonymous(locals, attrs, escape, rethrow, merge) {
-attrs = attrs || jade.attrs; escape = escape || jade.escape; rethrow = rethrow || jade.rethrow; merge = merge || jade.merge;
+this["JST"]["jade2"] = function anonymous(locals) {
 var buf = [];
 with (locals || {}) {
-var interp;
-buf.push('<div id="test" class="test"><span id="data">data</span>');
+buf.push("<div id=\"test\" class=\"test\"><span id=\"data\">data</span>");
 if ( test)
 {
-buf.push('<div>testing 2</div>');
+buf.push("<div>testing 2</div>");
 }
-buf.push('</div>');
+buf.push("</div>");
 }
 return buf.join("");
 };
