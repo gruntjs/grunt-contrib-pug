4e5889397eb50240040dcb3f9c5eac6d554ae070
Jaime Pillora <jpillora@gmail.com>
2013-05-08 11:53:52 +1000

    for travis (fails locally)

--- test/expected/jst/jadeInclude.js
+++ test/expected/jst/jadeInclude.js
@@ -4,7 +4,7 @@ this["JST"]["jadeInclude"] = function anonymous(locals) {
 var buf = [];
 with (locals || {}) {
 buf.push("<html><head><title>TEST</title></head><body></body></html>");
- var a = 'hello jade test'
+var a = 'hello jade test'
 buf.push("<p>" + (jade.escape(null == (jade.interp = a) ? "" : jade.interp)) + "</p>");
 }
 return buf.join("");
