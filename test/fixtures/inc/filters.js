f76021fbddc1e324700e5f2e1c56a16a98cef9e5
Vladimir Starkov <vstarkov@yandex-team.ru>
2013-09-22 07:29:07 +0600

    simplified base tests for filters #50

--- test/fixtures/inc/filters.js
+++ test/fixtures/inc/filters.js
@@ -1,9 +1,9 @@
 var jadefilters = module.exports = {};
 
 jadefilters.some = function(block) {
-	return '<some>'+block+'</some>';
+	return 'some: ' + block;
 };
 
 jadefilters.another = function(block) {
-	return '<another>'+block+'</another>';
+	return 'another: ' + block;
 };
