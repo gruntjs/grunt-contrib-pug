dd3c8088100c4c787ae3612d6321d084d8a2f2bc
perfectsquircle <calvinfurano@gmail.com>
2014-02-27 17:24:55 -0700

    Modifiying expected output to be compatible with jade 1.2

--- test/expected/amd/jadeInclude.js
+++ test/expected/amd/jadeInclude.js
@@ -3,10 +3,11 @@ define(['jade'], function(jade) { if(jade && jade['runtime'] !== undefined) { ja
 return function template(locals) {
 var buf = [];
 var jade_mixins = {};
+var jade_interp;
 
 buf.push("<html><head><title>TEST</title></head><body></body></html>");
 var a = 'hello jade test'
-buf.push("<p>" + (jade.escape(null == (jade.interp = a) ? "" : jade.interp)) + "</p>");;return buf.join("");
+buf.push("<p>" + (jade.escape(null == (jade_interp = a) ? "" : jade_interp)) + "</p>");;return buf.join("");
 }
 
 });
--- test/expected/amd/jadeTemplate.js
+++ test/expected/amd/jadeTemplate.js
@@ -3,8 +3,9 @@ define(['jade'], function(jade) { if(jade && jade['runtime'] !== undefined) { ja
 return function template(locals) {
 var buf = [];
 var jade_mixins = {};
+var jade_interp;
 var locals_ = (locals || {}),year = locals_.year;
-buf.push("<div>" + (jade.escape(null == (jade.interp = year) ? "" : jade.interp)) + "</div>");;return buf.join("");
+buf.push("<div>" + (jade.escape(null == (jade_interp = year) ? "" : jade_interp)) + "</div>");;return buf.join("");
 }
 
 });
