28d9cdeedd060144abbc8fd85e1bcc9f71d784d8
Kyle Robinson Young <kyle@dontkry.com>
2014-08-27 13:44:36 -0700

    Normalize the line endings in the tests

--- test/jade_test.js
+++ test/jade_test.js
@@ -1,45 +1,49 @@
 var grunt = require('grunt');
 
+var read = function(src) {
+  return grunt.util.normalizelf(grunt.file.read(src));
+};
+
 exports.jade = {
   compile: function(test) {
     'use strict';
 
     test.expect(9);
 
-    var actual = grunt.file.read('tmp/jade.html');
-    var expected = grunt.file.read('test/expected/jade.html');
+    var actual = read('tmp/jade.html');
+    var expected = read('test/expected/jade.html');
     test.equal(expected, actual, 'should compile jade templates to html');
 
-    actual = grunt.file.read('tmp/jade2.html');
-    expected = grunt.file.read('test/expected/jade2.html');
+    actual = read('tmp/jade2.html');
+    expected = read('test/expected/jade2.html');
     test.equal(expected, actual, 'should compile jade templates to html (multiple files support)');
 
-    actual = grunt.file.read('tmp/jadeInclude.html');
-    expected = grunt.file.read('test/expected/jadeInclude.html');
+    actual = read('tmp/jadeInclude.html');
+    expected = read('test/expected/jadeInclude.html');
     test.equal(expected, actual, 'should compile jade templates to html with an include');
 
-    actual = grunt.file.read('tmp/jadeTemplate.html');
-    expected = grunt.file.read('test/expected/jadeTemplate.html');
+    actual = read('tmp/jadeTemplate.html');
+    expected = read('test/expected/jadeTemplate.html');
     test.equal(expected, actual, 'should compile jade templates to html with grunt template support');
 
-    actual = grunt.file.read('tmp/jadeDynamicData.html');
-    expected = grunt.file.read('test/expected/jadeDynamicData.html');
+    actual = read('tmp/jadeDynamicData.html');
+    expected = read('test/expected/jadeDynamicData.html');
     test.equal(expected, actual, 'should allow options.data to be a function');
 
-    actual = grunt.file.read('tmp/inlineFilters.html');
-    expected = grunt.file.read('test/expected/jadeFilters.html');
+    actual = read('tmp/inlineFilters.html');
+    expected = read('test/expected/jadeFilters.html');
     test.equal(expected, actual, 'should compile jade with inline filters');
 
-    actual = grunt.file.read('tmp/exportedFilters.html');
-    expected = grunt.file.read('test/expected/jadeFilters.html');
+    actual = read('tmp/exportedFilters.html');
+    expected = read('test/expected/jadeFilters.html');
     test.equal(expected, actual, 'should compile jade with exported filters');
 
-    actual = grunt.file.read('tmp/jadeAdvancedFilters.html');
-    expected = grunt.file.read('test/expected/jadeAdvancedFilters.html');
+    actual = read('tmp/jadeAdvancedFilters.html');
+    expected = read('test/expected/jadeAdvancedFilters.html');
     test.equal(expected, actual, 'should compile jade with nested filters with access to locals');
 
-    actual = grunt.file.read('tmp/jadeUsingmixin.html');
-    expected = grunt.file.read('test/expected/jadeUsingmixin.html');
+    actual = read('tmp/jadeUsingmixin.html');
+    expected = read('test/expected/jadeUsingmixin.html');
     test.equal(expected, actual, 'should compile jade with nested mixins');
 
     test.done();
