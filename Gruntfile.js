3141435a7fbfe60ba02f2d31579595291756fd48
Radko Dinev <radko.dinev@gmail.com>
2014-08-30 08:46:13 +0300

    First check code with jshint upon running npm/grunt test

--- Gruntfile.js
+++ Gruntfile.js
@@ -162,9 +162,9 @@ module.exports = function(grunt) {
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'jade', 'nodeunit']);
+  grunt.registerTask('test', ['jshint', 'clean', 'jade', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test', 'build-contrib']);
+  grunt.registerTask('default', ['test', 'build-contrib']);
 
 };
