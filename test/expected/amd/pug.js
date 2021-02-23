3a81be47010f953cb436da8b149ed8676b6fb072
Arek W <arek01@gmail.com>
2021-02-23 03:06:10 +0000

    Update to pug 3.0.0 & drop Node.js < 10
    Also updates development dependencies

--- test/expected/amd/pug.js
+++ test/expected/amd/pug.js
@@ -1,9 +1,17 @@
 define(['pug'], function(pug) { if(pug && pug['runtime'] !== undefined) { pug = pug.runtime; }
 
-return function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;var locals_for_with = (locals || {});(function (test) {pug_html = pug_html + "\u003Cdiv class=\"test\" id=\"test\"\u003E\u003Cspan id=\"data\"\u003Edata\u003C\u002Fspan\u003E";
+return function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;
+    var locals_for_with = (locals || {});
+    
+    (function (test) {
+      pug_html = pug_html + "\u003Cdiv class=\"test\" id=\"test\"\u003E\u003Cspan id=\"data\"\u003Edata\u003C\u002Fspan\u003E";
 if (test) {
 pug_html = pug_html + "\u003Cdiv\u003Etesting\u003C\u002Fdiv\u003E";
 }
-pug_html = pug_html + "\u003C\u002Fdiv\u003E";}.call(this,"test" in locals_for_with?locals_for_with.test:typeof test!=="undefined"?test:undefined));;return pug_html;}
+pug_html = pug_html + "\u003C\u002Fdiv\u003E";
+    }.call(this, "test" in locals_for_with ?
+        locals_for_with.test :
+        typeof test !== 'undefined' ? test : undefined));
+    ;;return pug_html;}
 
 });
--- test/expected/jst/pug.js
+++ test/expected/jst/pug.js
@@ -1,7 +1,15 @@
 this["JST"] = this["JST"] || {};
 
-this["JST"]["pug"] = function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;var locals_for_with = (locals || {});(function (test) {pug_html = pug_html + "\u003Cdiv class=\"test\" id=\"test\"\u003E\u003Cspan id=\"data\"\u003Edata\u003C\u002Fspan\u003E";
+this["JST"]["pug"] = function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;;
+    var locals_for_with = (locals || {});
+    
+    (function (test) {
+      pug_html = pug_html + "\u003Cdiv class=\"test\" id=\"test\"\u003E\u003Cspan id=\"data\"\u003Edata\u003C\u002Fspan\u003E";
 if (test) {
 pug_html = pug_html + "\u003Cdiv\u003Etesting\u003C\u002Fdiv\u003E";
 }
-pug_html = pug_html + "\u003C\u002Fdiv\u003E";}.call(this,"test" in locals_for_with?locals_for_with.test:typeof test!=="undefined"?test:undefined));;return pug_html;};
+pug_html = pug_html + "\u003C\u002Fdiv\u003E";
+    }.call(this, "test" in locals_for_with ?
+        locals_for_with.test :
+        typeof test !== 'undefined' ? test : undefined));
+    ;;return pug_html;};
